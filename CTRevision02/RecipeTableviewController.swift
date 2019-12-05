eacaf3007d78e5b4b0d9a70a8f01da47b988a7ec
ck <s10189088@connect.np.edu.sg>
2019-12-05 17:32:11 +0800

    5 December

---
+++ CTRevision02/RecipeTableviewController.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeTableviewController.swift
+//  CTRevision02
+//
+//  Created by MAD2_P02 on 5/12/19.
+//  Copyright © 2019 MAD2_P02. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class RecipeTableViewController : UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ CTRevision02/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CTRevision02
+//
+//  Created by MAD2_P02 on 5/12/19.
+//  Copyright © 2019 MAD2_P02. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
